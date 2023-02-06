c99a6a77247c236baa60987f540c2893b0c1afab
George Stamatiou <s6gestam@uni-bonn.de>
2023-02-06 12:56:08 +0100

    Add wasserstein distance test

--- pd_alg.py
+++ pd_alg.py
@@ -19,27 +19,29 @@ def power_iteration(K, x_0, tol = 1e-3, num_iterations = 10000000, verbose = Fal
 
     return bb, norm(K(bb))/norm(bb)
 
-def pd_alg(proxF, proxG, K, Kstar, tau, sigma, x0, y0, tol = 1e-2, max_iter = 1e6, verbose = False):
-	y = y0
-	x = x0
-	x_bar = x0
-	
-	difference = tol + 1
-	it = 0
+def pd_alg(proxF, proxG, K, Kstar, tau, sigma, x0, y0, tol = 1e-2, 
+           max_iter = 1e6, verbose = False):
+    y = y0
+    x = x0
+    x_bar = x0
+    
+    difference = tol + 1
+    it = 0
+    while (it <= max_iter and difference > tol):
+        yy = proxF(y + sigma * K(x_bar))
+        xx = proxG(x - tau * Kstar(yy))
+        xx_bar = 2 * xx - x
 
-	while (it <= max_iter and difference > tol):
-		yy = proxF(y + sigma * K(x_bar))
-		xx = proxG(x - tau * Kstar(yy))
-		xx_bar = 2 * xx - x
+        difference = np.max(np.abs(xx - x))
+        it += 1
 
-		difference = np.max(np.abs(xx - x))
-		it += 1
+        x = xx
+        x_bar = xx_bar
+        y = yy
 
-		x = xx
-		x_bar = xx_bar
-		y = yy
-
-		if verbose:
-			print(difference)
-
-	return x
+        if verbose:
+            print(difference)
+    
+    if it >= max_iter:
+        print("reached max iterations with diff:", difference)
+    return x
---
+++ ppoisson.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import time
+import graphlearning as gl
+from scipy.optimize import minimize, LinearConstraint
+from graph_utils import degrees
+from energy import penergy, jacobian
+
+class ppoisson():
+    def __init__(self, p, W):
+        self.p = p
+        self.W = W
+        self.n = W.shape[0]
+        self.u = None
+        self.fitted = False
+        self.predicted = False
+    
+    # train_ind: Indices of (few) labeled points
+    # train_labels: labels of labaled points (one hot encoding)
+    # start: starting point (n,k) array
+    def fit(self, train_ind, train_labels, start = np.zeros(1)):
+        if self.fitted:
+            return self.u
+        
+        start_time = time.time()
+
+        self.k = train_labels.shape[1]
+        d = degrees(self.W)
+        eye = np.eye(self.k)
+
+
+        if np.count_nonzero(start) == 0:
+            model = gl.ssl.poisson(self.W, solver='gradient_descent')
+            integer_coded_train_labels = np.argmax(train_labels, axis = 1)
+
+            start = model.fit(train_ind, integer_coded_train_labels) # model's fit 
+                                                                     # doesn't expect one hot encoding
+        
+
+        constrain_matrix = np.concatenate([d[i] * eye for i in range(self.n)], axis = 1)
+        linear_constraint = LinearConstraint(constrain_matrix, np.zeros(self.k), np.zeros(self.k))
+        
+        start = start.flatten()
+        res = minimize(penergy, x0 = start, args = (self.W, train_ind, train_labels, self.p), 
+                jac = jacobian, method = 'trust-constr', constraints = linear_constraint)
+        
+        self.u = res.x.reshape(self.n,self.k)
+        self.fitted = True
+        
+        end_time = time.time()
+        self.runtime = (end_time - start_time)/60 # in minutes
+
+        return self.u
+        
+    def predict(self):
+        if not self.fitted:
+            print("Not fitted yet")
+            return -1
+        if self.predicted:
+            return self.predictions
+        
+        self.predictions = np.argmax(self.u, axis = 1)
+        self.predicted = True
+
+        return self.predictions
+
+    
+    def fit_predict(self, train_ind, train_labels, start = np.zeros(1)):
+        self.fit(train_ind, train_labels, start)
+        
+        return self.predict()
+
+    # labels: integer valued
+    def accuracy(self, labels):
+        if not self.predicted:
+            self.predictions = self.predict()
+        
+        return 1 - np.count_nonzero(labels - self.predictions)/self.n
+
+    def print_info(self):
+        info_str = f"########### Gradient Descent (w/ Jacobian) for p = {self.p}\n"\
+                        f"\nAccuracy = {self.accuracy() * 100:.2f}%\n"\
+                        f"Runtime = {self.runtime:.2f} min"
+
+        print(info_str)
