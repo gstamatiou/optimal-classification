1b58877951b30b1a7264c897edfa604b1c7f72ae
George Stamatiou <s6gestam@uni-bonn.de>
2023-03-10 20:09:51 +0100

    Calculations with grid and non-singular kernel

--- ot_class/test_wass_2d_gaussians_grid.py
+++ ot_class/test_wass_2d_gaussians_grid.py
@@ -4,7 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from wass_dist import wass_dist
-from graph_utils import construct_weightmatrix, graph_grad
+from graph_utils import construct_weightmatrix, graph_grad, grad
 import ot
 import graphlearning as gl
 
@@ -13,39 +13,65 @@ def epsilon_matrix(X, epsilon):
     normed_diff = np.apply_along_axis(np.linalg.norm, 2, X[:, np.newaxis] - X)
     W = np.zeros((X.shape[0], X.shape[0]))
     
-    for i in range(n):
-        for j in range(n):
-            if normed_diff[i,j] <= epsilon:
-                W[i,j] = 1 - normed_diff[i,j]/epsilon
+    # for i in range(n):
+    #     for j in range(n):
+    #         if normed_diff[i,j] <= epsilon:
+    #             W[i,j] = 1 - normed_diff[i,j]/epsilon
     
-    #W[normed_diff <= epsilon] = 1/epsilon
+    W[normed_diff <= epsilon] = 1/epsilon
     
     return W
+
+def eta(t):
+    if t > 1:
+        return 0
+    else:
+        return 4*(1-t)
+
+def slow_epsilon_matrix(X, epsilon):
+    n = X.shape[0]
+    W = np.zeros((n,n))
     
-for n in np.arange(10, 35, 3)**2:
-    rn = np.sqrt(2)/(2*n)
-    sn = np.sqrt(rn)
-    print(f"n = {n}, rn = {rn}, s_n = {sn}")
-    xv, yv = np.meshgrid(np.linspace(0, 1, int(np.sqrt(n))), np.linspace(0, 1, int(np.sqrt(n))))
-    X = np.array([xv.flatten(), yv.flatten()]).T
+    for i in range(n):
+        for j in range(n):
+            W[i,j] = 1/epsilon*eta(np.linalg.norm(X[i]-X[j])/epsilon)
+            
+    return W
+
     
-    mu = np.zeros((n,1))
-    mu[0] = [1]
-    mu[-1] = [-1]
+for exponent in [1/9, 1/10, 1/11]:    
+    for n in np.arange(5, 41, 5)**2:
+        xv, yv = np.meshgrid(np.linspace(0, 1, int(np.sqrt(n))), np.linspace(0, 1, int(np.sqrt(n))))
+        X = np.array([xv.flatten(), yv.flatten()]).T
     
-    W = epsilon_matrix(X, sn)
-    #W = construct_weightmatrix(X)
-    #W = gl.weightmatrix.epsilon_ball(X, sn).toarray()
-    
-    
-    u0 = np.random.rand(n,1)
-    u2d = wass_dist(W, mu, u0, -graph_grad(u0, W), tol = 1e-4, 
-                    max_iter=1e5, verbose=False)
-    print(f"max_grad = {np.abs(graph_grad(u2d, W)).max()}")
-    
-    fig_pot = plt.figure()
-    ax_pot = fig_pot.add_subplot(projection='3d')
-    ax_pot.scatter(X[:, 0], X[:, 1], u2d, s = 2)
-    plt.show()
-    
-    print(f"cost: {(u2d * mu).sum()}")
+        rn = np.sqrt(2*np.linalg.norm(X[0] - X[1])**2)/2
+        sn = rn**exponent
+        
+        print(f"n = {n}\t rn = {rn:.2f}\t s_n = {sn:.2f} = rn^{np.log(sn)/np.log(rn):.2f}\t", 
+              end=' ')
+        
+        mu = np.zeros((n,1))
+        mu[0] = [1]
+        mu[-1] = [-1]
+        
+        W = slow_epsilon_matrix(X, sn)
+        #W = construct_weightmatrix(X)
+        #W = gl.weightmatrix.epsilon_ball(X, sn).toarray()
+        num_neighbors = np.count_nonzero(W[int(n/2)])
+        if num_neighbors == 1:
+            sn = 3 * sn
+            
+        print(f"neighbors = {np.count_nonzero(W[int(n/2)])}\t", end='')
+        
+        u0 = np.random.rand(n,1)
+        u2d = wass_dist(W, mu, u0, -grad(u0), tol = 1e-6, 
+                        max_iter=1e5, verbose=False)
+        #print(f"max_grad = {np.abs(graph_grad(u2d, W)).max()} ", end = '')
+        
+        fig_pot = plt.figure()
+        ax_pot = fig_pot.add_subplot(projection='3d')
+        ax_pot.scatter(X[:, 0], X[:, 1], u2d, s = 2)
+        plt.show()
+        
+        print(f"cost: {(u2d * mu).sum():.4f}")
+        
--- ot_class/wass_dist.py
+++ ot_class/wass_dist.py
@@ -4,7 +4,7 @@
 import numpy as np
 from numpy.linalg import norm
 from numpy.random import rand
-from graph_utils import graph_grad, graph_div
+from graph_utils import graph_grad, graph_div, grad
 from pd_alg import power_iteration, pd_alg
 
 def proxF(V, sigma):
@@ -26,7 +26,7 @@ def wass_dist(W, mu, u0, V0, tol = 1e-2, max_iter = 1e5, verbose = False, rate =
     Kstar = lambda V: -graph_div(V, W)
     Kstar_K = lambda u: Kstar(K(u))
     
-    _, eig = power_iteration(Kstar_K, rand(n), tol = 1e-2, verbose = False)
+    _, eig = power_iteration(Kstar_K, rand(n,1) * 10, tol = 1e-2, verbose = False)
     K_norm = np.sqrt(eig)
     sigma = rate/ (2 * K_norm)
     tau = rate/(2 * K_norm )
